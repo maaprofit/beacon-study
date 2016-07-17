6ba491ce3dd85477b8373bb6da32efc45a172c47
Marcelo Augusto <marcelo.augusto@metractive.com>
2016-07-17 12:50:42 -0300

    arduino ready to deploy HM-10

--- beacon-hm-10/beacon-hm-10.ino
+++ beacon-hm-10/beacon-hm-10.ino
@@ -2,14 +2,19 @@
 
 SoftwareSerial mySerial(7, 8);
 
+// Serial value
+String val;
+
+// Bauds for detectBleBaudRate function
 long bauds[] = {
     // major 
     9600, 57600, 115200,
     
     // others
     19200, 38400, 4800, 2400, 1200, 230400
-  };
+};
 
+// Detect BLE Baud Rate
 bool detectBleBaudRate() {
   Serial.println("Detecting BLE baud rate:");
   
@@ -26,6 +31,7 @@ bool detectBleBaudRate() {
     
     if (response == "OK") {
       Serial.println("Detected");
+      Serial.print(response);
       return true;
     } else {
       mySerial.end();
@@ -35,23 +41,28 @@ bool detectBleBaudRate() {
   detectBleBaudRate();
 }
 
+// Uncomment function to wake up bluetooth or discover BLE Baud Rate
 void setup() {
-  mySerial.begin(19200);
+  mySerial.begin(9600);
   Serial.begin(9600);
   // detectBleBaudRate();
 }
 
+// Reads strings that are sent to mySerial
 void loop() {
-  char c;
-  if (Serial.available()) {
-    c = Serial.read();
-    mySerial.write(c);
-    delay(50);
-    Serial.write(mySerial.read());
+  while (Serial.available() > 0) {
+     delay(10); // Delay needed to recognize key and value properly
+     String val = Serial.readString();
+     Serial.print("Serial sent: ");
+     Serial.println(val);
+     
+     mySerial.print(val);
+     mySerial.flush();
+
+     delay(50);
+
+     String response = mySerial.readString();
+     Serial.print("mySerial received: ");
+     Serial.println(response);
   }
-  
-  // if (Serial.available()) {
-    // c = mySerial.read();
-    // Serial.write(c);
-  // }
 }
